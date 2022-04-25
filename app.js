a1660d16ce64fc5bbd34d73011fa6097c6f321f8
asfelizzola <asfelizzola@gmail.com>
2022-04-24 22:52:18 -0300

    const eventRouter app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const categoryRouter = require('./routes/categories');
+const eventRouter = require('./routes/events');
 
 var app = express();
 
