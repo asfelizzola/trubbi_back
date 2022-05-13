54f067a1d961516004d7ec55f27396768b209290
asfelizzola <asfelizzola@gmail.com>
2022-05-13 14:08:44 -0300

    events

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/categories', categoryRouter);
 app.use('/api/events', eventRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
