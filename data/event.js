1a88b06230f3ca48a3720f2e58a4e0cbe114f2a0
asfelizzola <asfelizzola@gmail.com>
2022-05-14 17:21:40 -0300

    getEvents & getEvent - events details

--- data/event.js
+++ data/event.js
@@ -1,6 +1,7 @@
 const connection = require('./connection')
 const DATABASE = "trubbi_test"
 const COLLECTION_EVENTS = "events_test"
+const { ObjectId } = require('mongodb');
 
 async function getEvents(){
     const clientmongo = await connection.getConnection();
@@ -11,4 +12,13 @@ async function getEvents(){
     return events;                            
 }
 
-module.exports = {getEvents}
+async function getEvent(id){
+    const clientmongo = await connection.getConnection();
+    const event = await clientmongo
+                        .db(DATABASE)
+                        .collection(COLLECTION_EVENTS)
+                        .findOne({_id: new ObjectId(id)});
+    return event;
+}
+
+module.exports = {getEvents, getEvent}
--- routes/events.js
+++ routes/events.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data/event')
+const data = require('../data/event.js')
 
 router.get('/', async (req, res) => {
   let events = await data.getEvents();
   res.json(events);
 });
+
+router.get("/:id", async(req,res)=>{
+  try {
+    let events = await data.getEvent(req.params.id);
+    events ? res.json(events) : res.status(404).json({});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({});
+  }
+})
 
 module.exports = router;
